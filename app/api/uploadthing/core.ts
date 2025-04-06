61e2671d9475bbba85b7f52f9d1996ff4bb8c1ff
copturehub <accounts@copture.com>
2025-04-06 23:27:53 +0200

    Update core.ts

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,25 +1,15 @@
-// app/api/uploadthing/core.ts
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { type Metadata } from "uploadthing/server";
 
 const f = createUploadthing();
 
-type Metadata = {
-  pilotName: string;
-  projectName: string;
-};
-
 export const ourFileRouter = {
   pilotUploader: f(["image", "video", "text", "pdf", "audio"])
-    .input<Metadata>() // ✅ Metadata typad
+    .input<Metadata, { pilotName: string; projectName: string }>()
     .onUploadComplete(({ file, metadata }) => {
       console.log("✅ Upload complete!");
       console.log("Filename:", file.name);
-
-      if (metadata) {
-        console.log("Pilot:", metadata.pilotName);
-        console.log("Project:", metadata.projectName);
-      } else {
-        console.log("⚠️ No metadata received.");
-      }
+      console.log("Pilot:", metadata?.pilotName);
+      console.log("Project:", metadata?.projectName);
     }),
 } satisfies FileRouter;
