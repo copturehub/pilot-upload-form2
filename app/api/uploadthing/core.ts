c7a14f152be15ba46050459a8a3a4b329b1ff330
copturehub <accounts@copture.com>
2025-04-06 23:02:34 +0200

    Update core.ts

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,22 +1,20 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { z } from "zod";
 
 const f = createUploadthing();
 
-type Metadata = {
-  pilotName: string;
-  projectName: string;
-};
-
 export const ourFileRouter = {
   pilotUploader: f(["image", "video", "text", "pdf", "audio"])
-    .input<Metadata, Metadata>(() => ({
-      pilotName: "placeholder",
-      projectName: "placeholder",
-    }))
+    .input(
+      z.object({
+        pilotName: z.string(),
+        projectName: z.string(),
+      })
+    )
     .onUploadComplete(({ file, metadata }) => {
       console.log("✅ Upload complete!");
       console.log("Filename:", file.name);
-      console.log("Pilot:", metadata?.pilotName);
-      console.log("Project:", metadata?.projectName);
+      console.log("Pilot:", metadata.pilotName);
+      console.log("Project:", metadata.projectName);
     }),
 } satisfies FileRouter;
