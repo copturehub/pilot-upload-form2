2805c75d6b334aff0cd9aa315769445c736814bd
copturehub <accounts@copture.com>
2025-04-06 19:13:10 +0200

    Update core.ts

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,12 +1,13 @@
 // app/api/uploadthing/core.ts
 
 import { createUploadthing, type FileRouter } from "uploadthing/server";
+import { z } from "zod";
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  pilotUploader: f(["image", "video", "text", "pdf", "audio"]) // giltiga MIME-kategorier
-    .input({ pilotName: String, projectName: String }) // typad metadata
+  pilotUploader: f(["image", "video", "text", "pdf", "audio"])
+    .input(z.object({ pilotName: z.string(), projectName: z.string() }))
     .onUploadComplete(({ file, metadata }) => {
       console.log("✅ Upload complete!");
       console.log("Filename:", file.name);
